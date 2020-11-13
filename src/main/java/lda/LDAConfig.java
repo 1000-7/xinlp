2d5f19302ae29f36ffadaf51352d43b679994d25
xavior.wx <xavior.wx@alibaba-inc.com>
2020-11-13 15:16:19 +0800

    vscode gittest

--- src/main/java/lda/LDAConfig.java
+++ src/main/java/lda/LDAConfig.java
@@ -5,5 +5,8 @@ public class LDAConfig {
     public static final String RESPATH = "/Users/unclewang/Idea_Projects/xinlp/src/main/resources/lda/result/";
     public static final String DOCPATH = "/Users/unclewang/Idea_Projects/xinlp/src/main/resources/lda/doc";
     public static final String PARAMPATH = "/Users/unclewang/Idea_Projects/xinlp/src/main/resources/lda/LdaParameters.txt";
+    public static void main(String[] args) {
+        System.out.println("x");
+    }
     
 }
